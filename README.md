434820069741490aea914b51f9078f21c98ef7b1
rudrag1410 <rudragupta1410@gmail.com>
2024-06-14 17:31:53 +0530

    intial commit

--- README.md
+++
@@ -1,3 +0,0 @@
-## url [https://shop.pudgypenguins.com/]
-
-# Figma https://www.figma.com/design/dNZG45EY8TzrSLH5187QQv/Untitled?node-id=0-1&t=q0JTvUNvm78spLGB-0
--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("hello world");
+window.onload = function () {
+   const announcementContent = document.querySelector('.announcementContent');
+
+   const contentClone = announcementContent.innerHTML;
+   announcementContent.innerHTML += contentClone;
+
+   gsap.set(".announcementContent", { xPercent: -2 });
+
+   // Start the scrolling animation
+   gsap.to(".announcementContent", {
+      xPercent: -50,
+      duration: 15, // Duration of the animation
+      ease: "linear",
+      repeat: -1, // Repeat the animation infinitely
+   });
+};
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+   var navIcon = document.getElementById('humBurgerIcon');
+   navIcon.addEventListener('click', function () {
+      navIcon.classList.toggle('open');
+   });
+});
