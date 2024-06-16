809dcb860c1044e3490ccad3aa84b0e36e0421e9
rudrag1410 <rudragupta1410@gmail.com>
2024-06-16 10:29:14 +0530

    Footer is added

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 window.onload = function () {
+   // Existing animation for announcementContent
    const announcementContent = document.querySelector('.announcementContent');
-
    const contentClone = announcementContent.innerHTML;
    announcementContent.innerHTML += contentClone;
 
    gsap.set(".announcementContent", { xPercent: -2 });
 
-   // Start the scrolling animation
+   // Start the scrolling animation for announcementContent
    gsap.to(".announcementContent", {
       xPercent: -50,
-      duration: 15, // Duration of the animation
+      duration: 15,
       ease: "linear",
-      repeat: -1, // Repeat the animation infinitely
+      repeat: -1,
+   });
+
+   // Setup for footer-marquee
+   const footerMarquee = document.querySelector('.footer-marquee');
+   const footerContentClone = footerMarquee.innerHTML;
+   footerMarquee.innerHTML += footerContentClone;
+
+   gsap.set(".footer-marquee", { xPercent: 5 });
+
+   // Start the scrolling animation for footer-marquee
+   gsap.to(".footer-marquee", {
+      xPercent: -20,
+      duration: 15, // Longer duration for slower effect
+      ease: "linear",
+      repeat: -1,
    });
 };
+
+
 
 
 
@@ -24,3 +41,23 @@ document.addEventListener('DOMContentLoaded', function () {
       navIcon.classList.toggle('open');
    });
 });
+
+
+
+
+window.onscroll = function () { myFunction() };
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+   if (window.pageYOffset > sticky) {
+      header.classList.add("sticky");
+   } else {
+      header.classList.remove("sticky");
+   }
+}
